8d8446b33d6bf1ccba2804da1222747d5d90ec39
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-04 12:23:25 -0500

    Config eslint to recognize jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: 'airbnb-base',
   overrides: [
   ],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',

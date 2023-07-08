c75917540bb182c078d48d1f4ff01cc50cdebbc6
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-07 22:44:19 -0500

    Place ships on random coords

--- src/components/game.js
+++ src/components/game.js
@@ -3,21 +3,21 @@ import Player from './player/player';
 import Gameboard from './gameboard/gameboard';
 import Ship from './ship/ship';
 
-const populateBoard = (gameboard, xCoords, yCoords) => {
+const getRandomCoord = (max) => Math.floor(Math.random() * max);
+
+const populateBoard = (gameboard) => {
   const ships = [new Ship(2), new Ship(3), new Ship(3), new Ship(4), new Ship(5)];
 
   ships.forEach((ship) => {
-    try {
-      if (!gameboard.placeShip(xCoords.shift(), yCoords.shift(), ship, true)) {
-        throw new Error('Incorrect ship placement');
-      }
-    } catch (e) {
-      console.log(e);
-    }
+    let legalPlacement = false;
+    do {
+      console.log('this was ran');
+      const xCoord = getRandomCoord(gameboard.maxRow);
+      const yCoord = getRandomCoord(gameboard.maxCol);
+      if (gameboard.placeShip(xCoord, yCoord, ship, true)) legalPlacement = true;
+    } while (!legalPlacement);
   });
 };
-
-const getRandomCoord = (max) => Math.floor(Math.random() * max);
 
 const isGameOver = (player, ai) => {
   if (player.gameboard.allShipsSunk()) {
@@ -34,15 +34,25 @@ const isGameOver = (player, ai) => {
 
 const game = () => {
   const player = new Player(true, new Gameboard(), 'player 1');
-  const playerXCoords = [0, 3, 6, 8, 9];
-  const playerYCoords = [0, 3, 6, 3, 3];
+
+  // const playerXCoords = [];
+  // const playerYCoords = [];
+  // for (let i = 0; i < player.gameboard.maxRow; i += 1) {
+  //   playerXCoords.push(getRandomCoord(player.gameboard.maxRow));
+  //   playerYCoords.push(getRandomCoord(player.gameboard.maxCol));
+  // }
 
   const ai = new Player(false, new Gameboard(), 'ai');
-  const aiXCoords = [0, 5, 9, 0, 4];
-  const aiYCoords = [0, 5, 6, 4, 5];
+  // const aiXCoords = [];
+  // const aiYCoords = [];
 
-  populateBoard(player.gameboard, playerXCoords, playerYCoords);
-  populateBoard(ai.gameboard, aiXCoords, aiYCoords);
+  // for (let i = 0; i < ai.gameboard.maxCol; i += 1) {
+  //   aiXCoords.push(getRandomCoord(ai.gameboard.maxRow));
+  //   aiYCoords.push(getRandomCoord(ai.gameboard.maxCol));
+  // }
+
+  populateBoard(player.gameboard);
+  populateBoard(ai.gameboard);
 
   PubSub.publish('players', { player1: player, player2: ai });
 };
